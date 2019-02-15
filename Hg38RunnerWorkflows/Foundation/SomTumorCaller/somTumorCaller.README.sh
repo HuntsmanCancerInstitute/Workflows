9b6b2bf87cb399f79696397390568c7a763e7372
DavidAustinNix <david.nix@hci.utah.edu>
2019-02-15 10:33:27 -0700

    Added reworked Foundation workflows based on Singularity

---
+++ Hg38RunnerWorkflows/Foundation/SomTumorCaller/somTumorCaller.README.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+#SBATCH --account=hci-rw
+#SBATCH --partition=hci-rw
+#SBATCH -N 1
+#SBATCH -t 48:00:00
+
+set -e; start=$(date +'%s'); rm -f FAILED COMPLETE QUEUED; touch STARTED
+
+# 7 February 2019
+# David.Nix@Hci.Utah.Edu
+# Huntsman Cancer Institute
+
+# This tumor capture panel workflow uses Illumina's Manta and Strelka2 variant callers to identify short INDELs and SNVs. It uses a high depth NA12878 bam file in place of a matched normal.  
+# A tuned set of filtering statistics is applied to produce lists with different FDR tiers. 
+# A panel of normals is used to remove systematic false positives due to high localized background. Lastly, a prior call frequency is calculated for each variant to identify call artifacts.
+
+#### Do just once ####
+
+# 1) Install Singularity (https://www.sylabs.io) or load via a module then define the path to the executable
+module load singularity/3.0.1
+singExec=/uufs/chpc.utah.edu/sys/installdir/singularity3/3.0.1/bin/singularity
+
+# 2) Define file paths to "mount" in the container. The first is to the TNRunner data bundle downloaded and uncompressed from https://hci-bio-app.hci.utah.edu/gnomex/gnomexFlex.jsp?analysisNumber=A5578 . The second is the path to your data.
+dataBundle=/uufs/chpc.utah.edu/common/PE/hci-bioinformatics1/TNRunner
+myData=/uufs/chpc.utah.edu/common/HIPAA/u0028003/Scratch
+
+# 3) Modify the workflow xxx.sing file setting the paths to the required resources. These must be within the mounts.
+
+# 4) Build the singularity container, and define the path to the xxx.sif file, do just once after each update.
+#$singExec pull docker://hcibioinformatics/public:SnakeMakeBioApps_4
+container=/uufs/chpc.utah.edu/common/HIPAA/u0028003/HCINix/SingularityBuilds/public_SnakeMakeBioApps_4.sif
+
+
+#### Do for every run ####
+
+# 1) Create a folder named as you would like the analysis name to appear, this along with the genome build will be prepended onto all files, no spaces, change into it. This must reside somewhere in the myData mount path.
+
+# 2) Soft link or move in the tumor bam and bai files.
+
+# 3) Copy over the workflow docs: xxx.sing, xxx.README.sh, and xxx.sm into the job directory.
+
+# 4) Launch the xxx.README.sh via sbatch or run it on your local server.  
+
+# 5) If the run fails, fix the issue and restart.  Snakemake should pick up where it left off.
+
+
+
+#### No need to modify anything below ####
+
+echo -e "\n---------- Starting -------- $((($(date +'%s') - $start)/60)) min"
+
+# Read out params
+name=${PWD##*/}
+jobDir=`readlink -f .`
+
+SINGULARITYENV_name=$name SINGULARITYENV_jobDir=$jobDir SINGULARITYENV_dataBundle=$dataBundle \
+$singExec exec --containall --bind $dataBundle,$myData $container \
+bash $jobDir/*.sing
+
+echo -e "\n---------- Complete! -------- $((($(date +'%s') - $start)/60)) min total"
+
+# Final cleanup
+mkdir -p RunScripts
+mv somTumorCaller* RunScripts/
+mv -f *.log  Logs/ || true
+mv -f slurm* Logs/ || true
+rm -rf .snakemake 
+rm -f FAILED STARTED DONE RESTART
+touch COMPLETE 
+
+
+
